3d870ed4c84823f5c709d781fa1058f1292261d8
Alan.Luna <aklt1994@gmail.com>
2025-08-25 00:59:37 +0200

    Fix: type error

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Modal from "./components/Modal";
-import MoviesList from "./components/MoviesList'
-import movies from './data/movies.json";
+import MoviesList from "./components/MoviesList";
+import movies from "./data/movies.json";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <MoviesList movies={movies} />
       <button onClick={() => setIsOpen(true)}>Open Modal</button>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <header>Header</header>
         <section>Body</section>
       </Modal>
+      <MoviesList movies={movies} />
     </>
   );
 }
