92765863b48f967b212d1ac413076352cd52e469
Alan.Luna <aklt1994@gmail.com>
2025-08-25 09:48:06 +0200

    Add localstorage functions

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,18 @@
+export function addToLocalStorage(key: string, value: unknown):void {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error("Error saving to localStorage", error);
+  }
+};
+
+export function getFromLocalStorage<T>(key: string): T | null {
+  try {
+    const item = localStorage.getItem(key);
+  
+    return item ? (JSON.parse(item) as T) : null;
+  } catch (error) {
+    console.error("Error reading from localStorage", error);
+    return null;
+  }
+}
