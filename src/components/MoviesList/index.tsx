d5e78f6bf002a5cac78f5e8ea7096566479be119
Milky Raven <heartonllamas@gmail.com>
2025-08-25 09:02:44 +0200

    chore: add carrousel and icons library

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -1,19 +1,51 @@
-import MovieCard from "../MovieCard"
-import type { Movie } from "../../types/Movie"
-import './styles.css'
+import { useEffect, useRef } from "react";
+import Glide from "@glidejs/glide";
+import MovieCard from "../MovieCard";
+import type { Movie } from "../../types/Movie";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import "./styles.css";
 
 interface MovieListProps {
-    movies: Movie[]
+    movies: Movie[];
 }
+
 function MoviesList({ movies }: MovieListProps) {
-    console.log(movies);
+    const glideRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (glideRef.current) {
+            const glide = new Glide(glideRef.current, {
+                type: "carousel",
+                startAt: 0,
+                perView: 3,
+                gap: 20,
+                // autoplay: 3000,
+                hoverpause: true,
+                focusAt: "center",
+                animationDuration: 800,
+            });
+
+            glide.mount();
+        }
+    }, []);
+
     return (
-        <div className="movies-list">
-            {movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-            ))}
+        <div className="glide" ref={glideRef}>
+            <div className="glide__track" data-glide-el="track">
+                <ul className="glide__slides">
+                    {movies.map((movie) => (
+                        <li className="glide__slide" key={movie.id}>
+                            <MovieCard movie={movie} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="glide__arrows" data-glide-el="controls">
+                <button className="glide__arrow glide__arrow--left" data-glide-dir="<"><IoIosArrowBack /></button>
+                <button className="glide__arrow glide__arrow--right" data-glide-dir=">"><IoIosArrowForward /></button>
+            </div>
         </div>
-    )
+    );
 }
 
 export default MoviesList
